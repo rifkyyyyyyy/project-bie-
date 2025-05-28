fa2533c5530ed17624f45eb4249aca985d7db293
Mochammad Rifky Alfiansyah <rfkalfiansyah12@gmail.com>
2025-05-28 13:11:00 +0700

    admin

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,13 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
     public function index()
     {
-        return view('login'); // Pastikan file resources/views/login.blade.php ada
+        return view('login'); // resources/views/login.blade.php
     }
 
     public function authenticate(Request $request)
@@ -21,29 +20,36 @@ class LoginController extends Controller
             'password' => ['required'],
         ]);
 
-               if (Auth::guard('web')->attempt($credentials)) {
-                \Log::info('Login berhasil');
-                $request->session()->regenerate();
+        // Coba login
+        if (Auth::guard('web')->attempt($credentials)) {
+            $request->session()->regenerate();
+
+            $user = Auth::user();
+            \Log::info('Login berhasil oleh user dengan level: ' . $user->level);
+
+            // Hanya admin yang boleh login
+            if ($user->level === 'admin') {
                 return redirect()->intended('/dashboard');
-}
+            } else {
+                Auth::logout();
+                abort(404); // Tampilkan halaman 404
+            }
+        }
 
-\Log::error('Login gagal');
-
-
+        \Log::error('Login gagal');
 
         // Jika gagal login
         return back()->withErrors([
             'email' => 'Email atau password salah.',
-        ])->onlyInput('email'); // agar email tetap terisi
+        ])->onlyInput('email');
     }
 
-   public function logout(Request $request)
-{
-    Auth::guard('web')->logout();
-    $request->session()->invalidate();
-    $request->session()->regenerateToken();
+    public function logout(Request $request)
+    {
+        Auth::guard('web')->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
 
-    return redirect('/login');
-}
-
+        return redirect('/login');
+    }
 }
